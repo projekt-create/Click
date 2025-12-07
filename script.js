003d420c7bdc12536446b3335f32358d24e760d1
kali <kali@kali.kali>
2025-12-06 20:57:58 -0600

    all section done

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 const btn = document.querySelector('.button');
 const video = document.querySelector('.video');
 const text = document.querySelector('.text');
+let x = 0;
 
 btn.addEventListener('click', () => {
-    video.style.display = 'block';
-    video.currentTime = 0;
-    video.play();
-    btn.style.display = 'none';
-    text.style.display = 'block';
+    if (x == 0) {
+        video.style.display = 'block';
+        video.currentTime = 0;
+        video.play();
+        btn.style.display = 'none';
+        text.style.display = 'block';
+        x++;
 
-    setTimeout(() => {
-        video.style.display = 'none';
-        video.pause();
-        btn.style.display = 'flex';
-        text.style.display = 'none';
-    }, 24000);
+        setTimeout(() => {
+            if (x === 1) {
+                video.style.display = 'none';
+                video.pause();
+                btn.style.display = 'flex';
+                text.style.display = 'none';
+            }
+        }, 60000);
+    }else{
+        video.src = './ZWabdmcVTx6ZIuRTrnQb+O0nQe98w_9Y.mp4';
+        video.style.display = 'block';
+        video.currentTime = 0;
+        video.play();
+        btn.style.display = 'none';
+        text.style.display = 'block';
+        x++;
+
+        setTimeout(() => {
+            if (x === 1) {
+                video.style.display = 'none';
+                video.pause();
+                btn.style.display = 'flex';
+                text.style.display = 'none';
+            }
+        }, 24000);
+    }
 });
 
 text.addEventListener('click', () => {
